5d88d0fa8ab69908c19593f045190610bfdfb165
Ahmedturaj <ahmedturaj12@gmail.com>
2024-03-27 20:10:06 +0600

    fixed the whole page's  responsive issue

--- src/components/Home/Book/Book.jsx
+++ src/components/Home/Book/Book.jsx
@@ -6,23 +6,23 @@ const Book = ({ book }) => {
     const { bookId, bookName, tags, category, author, image, rating } = book
     return (
         <Link to={`/bookDetails/${bookId}`}>
-            <div className="card w-96 bg-base-100 shadow-xl cursor-pointer">
-                <figure className="px-10 pt-10 pb-3 bg-[#F3F3F3]">
-                    <img style={{ transition: 'all .8s' }} src={image} alt={`picture of ${bookName}`} className="rounded-xl w-60 hover:scale-105 h-60" />
+            <div className="card w-full md:w-96 bg-base-100 shadow-xl cursor-pointer">
+                <figure className="px-5 md:px-10 pt-5 md:pt-10 pb-3 bg-[#F3F3F3]">
+                    <img style={{ transition: 'all .8s' }} src={image} alt={`picture of ${bookName}`} className="rounded-xl w-full md:w-60 hover:scale-105 h-60 md:h-auto" />
                 </figure>
-                <div className="card-body">
-                    <div className="flex gap-5">
-                        {
-                            tags.map((tag, ind) => <p style={{ transition: 'all .8s' }} className='text-xs rounded-3xl border px-2 border-[#23BE0A] text-[#23BE0A] cursor-pointer hover:bg-[#23BE0A] hover:text-white hover:text-sm hover:scale-105' key={ind}>{tag}</p>)
-                        }
+                <div className="card-body px-5 md:px-10">
+                    <div className="flex flex-wrap gap-2 md:gap-5">
+                        {tags.map((tag, ind) => (
+                            <p style={{ transition: 'all .8s' }} className='text-xs md:text-sm rounded-3xl border px-2 border-[#23BE0A] text-[#23BE0A] cursor-pointer hover:bg-[#23BE0A] hover:text-white hover:text-sm hover:scale-105' key={ind}>{tag}</p>
+                        ))}
                     </div>
-                    <h2 style={{ transition: 'all .5s' }} className="card-title hover:text-gray-400">{bookName}</h2>
-                    <p>By: {author}</p>
-                    <hr className='my-5' />
+                    <h2 style={{ transition: 'all .5s' }} className="card-title text-base md:text-lg hover:text-gray-400">{bookName}</h2>
+                    <p className="text-xs md:text-sm">By: {author}</p>
+                    <hr className='my-3 md:my-5' />
                     <div className="card-actions w-full">
-                        <div className='flex justify-between items-stretch w-full'>
-                            <p className='text-sm'>{category}</p>
-                            <p className='flex gap-2 justify-center items-center'><CiStar />{rating}</p>
+                        <div className='flex justify-between items-center'>
+                            <p className='text-xs md:text-sm'>{category}</p>
+                            <p className='flex gap-1 items-center text-xs md:text-sm'><CiStar />{rating}</p>
                         </div>
                     </div>
                 </div>
--- src/components/ListedBooks/ListedCard/ListedCard.jsx
+++ src/components/ListedBooks/ListedCard/ListedCard.jsx
@@ -1,10 +1,10 @@
 
 import PropTypes from 'prop-types';
-import { IoLocationSharp, IoPersonSharp} from "react-icons/io5";
+import { IoLocationSharp, IoPersonSharp } from "react-icons/io5";
 import { FaRegFile } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 const ListedCard = ({ book }) => {
-    const {bookId, bookName, tags, category, author, image, rating,  totalPages, publisher, yearOfPublishing } = book;
+    const { bookId, bookName, tags, category, author, image, rating, totalPages, publisher, yearOfPublishing } = book;
     return (
         <div className='mb-8'>
             <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -13,21 +13,21 @@ const ListedCard = ({ book }) => {
                     <h2 className="card-title text-3xl">{bookName}</h2>
                     <p className='text-xl'>By: {author}</p>
                     <div className="flex flex-col md:flex-row items-start justify-start gap-5">
-                    <div className="flex gap-5">
-                                <span className="font-bold text-xl">Tag</span> {
-                                    tags.map((tag, ind) => <p style={{ transition: 'all .6s' }} className='text-xs rounded-3xl border px-2 border-[#23BE0A] text-[#23BE0A] cursor-pointer hover:bg-[#23BE0A] hover:text-white hover:text-sm hover:scale-105 flex items-center justify-center' key={ind}>#{tag}</p>)
-                                }
-                            </div>
-                            <div>
-                                <p className='flex justify-center items-center gap-3'><IoLocationSharp></IoLocationSharp> Year of Publishing: {yearOfPublishing}</p>
-                            </div>
+                        <div className="flex flex-col md:flex-row gap-5">
+                            <span className="font-bold text-xl">Tag</span> {
+                                tags.map((tag, ind) => <p style={{ transition: 'all .6s' }} className='text-xs rounded-3xl border px-2 border-[#23BE0A] text-[#23BE0A] cursor-pointer hover:bg-[#23BE0A] hover:text-white hover:text-sm hover:scale-105 flex items-center justify-center' key={ind}>#{tag}</p>)
+                            }
+                        </div>
+                        <div>
+                            <p className='flex justify-center items-center gap-3'><IoLocationSharp></IoLocationSharp> Year of Publishing: {yearOfPublishing}</p>
+                        </div>
                     </div>
-                    <div className="flex items-start justify-start gap-5">
-                    <p className='flex justify-start items-center gap-3'><IoPersonSharp></IoPersonSharp> publisher:{publisher}</p>
-                    <p className='flex justify-start items-center gap-3'><FaRegFile></FaRegFile> Pages:
-                    {totalPages}</p>
-                            </div>
-                            <hr className='my-5' />
+                    <div className="flex flex-col md:flex-row items-start justify-start gap-5">
+                        <p className='flex justify-start items-center gap-3'><IoPersonSharp></IoPersonSharp> publisher:{publisher}</p>
+                        <p className='flex justify-start items-center gap-3'><FaRegFile></FaRegFile> Pages:
+                            {totalPages}</p>
+                    </div>
+                    <hr className='my-5' />
                     <div className="card-actions justify-start">
                         <span className="btn text-[#328EFF] bg-[#328EFF26] rounded-3xl">Category: {category}</span>
                         <span className="btn text-[#FFAC33] bg-[#FFAC3326] rounded-3xl">Rating: {rating}</span>
